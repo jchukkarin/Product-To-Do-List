ad13e204fe46e8e0750979a8d7bae27907b8a419
่jchukkarin <jchukkarin@gmail.com>
2026-05-08 04:57:24 +0700

    Skill Test

--- app/dashboard/task/page.tsx
+++ app/dashboard/task/page.tsx
@@ -89,17 +89,17 @@ export default function TaskPage() {
                 </div>
 
                 <span className={`rounded-full px-3 py-1 text-xs font-semibold shadow-sm
-    ${
-      task.status === "TODO"
-        ? "bg-amber-100 text-amber-700"
-        : task.status === "DOING"
-        ? "bg-sky-100 text-sky-700"
-        : task.status === "DONE"
-        ? "bg-emerald-100 text-emerald-700"
-        : "bg-slate-100 text-slate-700"
-    }
-  `}
->
+                  ${
+                    task.status === "TODO"
+                    ? "bg-amber-100 text-amber-700"
+                    : task.status === "DOING"
+                    ? "bg-sky-100 text-sky-700"
+                    : task.status === "DONE"
+                    ? "bg-emerald-100 text-emerald-700"
+                    : "bg-slate-100 text-slate-700"
+                    }
+                  `}
+                  >
                   {task.status}
                 </span>
               </div>
